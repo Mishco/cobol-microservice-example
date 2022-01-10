edfa1116536f785f6fb5642b9ff312699214c02d
Michal Slovik <Michal.Slovik@EXXETA.sk>
2022-01-10 10:27:14 +0100

    fix cbl

--- src/fib.cbl
+++ src/fib.cbl
@@ -1,23 +1,23 @@
-IDENTIFICATION DIVISION.
-PROGRAM-ID. FIB.
-DATA DIVISION.
-FILE SECTION. 
-WORKING-STORAGE SECTION.
-01     N0          BINARY-C-LONG VALUE 0.
-01     N1          BINARY-C-LONG VALUE 1.
-01     SWAP        BINARY-C-LONG VALUE 1.
-01     RESULT      PIC Z(20)9.
-01     I           BINARY-C-LONG VALUE 0.
-01     I-MAX       BINARY-C-LONG VALUE 0.
-01     LARGEST-N   BINARY-C-LONG VALUE 92.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. FIB.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       01     N0          BINARY-C-LONG VALUE 0.
+       01     N1          BINARY-C-LONG VALUE 1.
+       01     SWAP        BINARY-C-LONG VALUE 1.
+       01     RESULT      PIC Z(20)9.
+       01     I           BINARY-C-LONG VALUE 0.
+       01     I-MAX       BINARY-C-LONG VALUE 0.
+       01     LARGEST-N   BINARY-C-LONG VALUE 92.
 
-PROCEDURE DIVISION.
-*>     this is where the labels get called
-       PERFORM MAIN
-       PERFORM ENDFIB 
-       GOBACK.
-    
-*>     this accepts input and determines the output    
+       PROCEDURE DIVISION.
+       *>     this is where the labels get called
+              PERFORM MAIN
+              PERFORM ENDFIB
+              GOBACK.
+       
+       *>     this accepts input and determines the output
        MAIN.
             DISPLAY "ENTER N TO GENERATE THE FIBONACCI SEQUENCE"
             ACCEPT I-MAX.
@@ -25,24 +25,24 @@ PROCEDURE DIVISION.
             EVALUATE TRUE
               WHEN I-MAX > LARGEST-N
                  PERFORM INVALIDN
-       
+
               WHEN I-MAX > 2
                  PERFORM CASEGREATERTHAN2
-       
+
               WHEN I-MAX = 2
                  PERFORM CASE2
-       
+
               WHEN I-MAX = 1
                  PERFORM CASE1
-       
+
               WHEN I-MAX = 0
                  PERFORM CASE0
-       
+
               WHEN OTHER
                  PERFORM INVALIDN
-       
+
             END-EVALUATE.
-       
+
             STOP RUN.
 
        *>  THE CASE FOR WHEN N = 0
